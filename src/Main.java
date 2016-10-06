482cfe187c420ffa8d2a4141e2fd6bdf105f474e
owner90 <owner90@192.168.5.3>
2016-10-06 15:58:52 +0300

    Два символни низа (string)конкатениран string със „_“

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Напишете програма, която приема 2 символни низа (string) 
+		//като вход, и изведете като резултат конкатениран string със „_“ между двата входни низа.
+
+String str1 = "Gogo";
+String str2 = "Mitko";
+String str3 = (str1 + " e gadje na " +str2);
+
+System.out.println(str3);
+
+	}
+
+}
